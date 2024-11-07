27bb1ac9f6c6d720c37e42f720c8356b94073c91
Brennan D Boatwright <brennandboatwright@gmail.com>
2024-11-06 20:04:21 -0600

    Sprint 3: Added a JavaScript file for user login

---
+++ login.js
@@ -0,0 +1,31 @@
+
+function liElement(inputString) {
+    return "<li>" + inputString + "</li>\n";
+}
+
+function FormLogin() {
+    const fd = FormData(document.getElementById("login-form"))
+    var fddiv = document.getElementById("login-form-div");
+    fetch("/user/" + fd.get("username") + "/login")
+    .then(
+        response => {
+            if (fddiv.display == "block") {
+                document.getElementById("login-form-div").display == "none";
+            }
+            // include a "logged in as ___" message as well
+            profileInfo.innerHTML = liElement("A");
+            profileInfo.innerHTML += liElement("B");
+            profileInfo.innerHTML += liElement("C");
+            profileInfo.innerHTML += liElement("D");
+            profileInfo.innerHTML += liElement("E");
+        }
+    ).catch((e) => {
+        console.error(e.message);
+    })
+}
+
+const profileInfo = document.getElementById("profileinfo");
+
+
+
+FormLogin();
