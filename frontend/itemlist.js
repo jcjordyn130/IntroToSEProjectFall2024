e700a387d18ba7de3ef4fd444931bc17efc8de00
Brennan D Boatwright <brennandboatwright@gmail.com>
2024-11-17 15:15:56 -0600

    Sprint 3: Small change to itemlist.js

--- frontend/itemlist.js
+++ frontend/itemlist.js
@@ -5,9 +5,9 @@ function approveItem(id) {
     fetch(absoluteUrl + `item/${id}/unapprove`)
     .then(
         response => {
-            document.getElementById("button2").style.display = "none";
-            document.getElementById("button1").onclick = `console.log("Item Already Approved!")`;
-            document.getElementById("button1").innerText = "Item Approved";
+            document.getElementById("button2-" + id).style.display = "none";
+            document.getElementById("button1-" + id).onclick = `console.log("Item Already Approved!")`;
+            document.getElementById("button1-" + id).innerText = "Item Approved";
         }
     )
     .catch(
@@ -21,9 +21,9 @@ function unapproveItem(id) {
     fetch(absoluteUrl + `item/${id}/approve`)
     .then(
         response => {
-            document.getElementById("button1").style.display = "none";
-            document.getElementById("button2").onclick = `console.log("Item Already Unapproved!")`;
-            document.getElementById("button2").innerText = "Item Unapproved";
+            document.getElementById("button1-" + id).style.display = "none";
+            document.getElementById("button2-" + id).onclick = `console.log("Item Already Unapproved!")`;
+            document.getElementById("button2-" + id).innerText = "Item Unapproved";
         }
     ).catch(
         (e) => {
@@ -48,8 +48,8 @@ function GenerateItemList() {
                     itemListText += `<li>ID: ${item.id}</li>\n`;
                     itemListText += `<li>Approved: ${item.approval}</li>\n`;
                     itemListText += "</ul>\n<br/>\n";
-                    itemListText += `<button type="button" id="button1" onclick="approveItem(${item.id})">Approved?</button>\n`;
-                    itemListText += `<button type="button" id="button2" onclick="unapproveItem(${item.id})>Unapproved?</button>\n`;
+                    itemListText += `<button type="button" id="button1-${item.id}" onclick="approveItem(${item.id})">Approved?</button>\n`;
+                    itemListText += `<button type="button" id="button2-${item.id}" onclick="unapproveItem(${item.id})>Unapproved?</button>\n`;
                     itemListText += "<br/>\n";
                 }
                 itemListElement.innerHTML = itemListText;
