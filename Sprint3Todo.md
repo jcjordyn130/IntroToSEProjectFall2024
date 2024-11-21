89ccb40523e05dc233a49232bae20bf8a194ad89
Brennan D Boatwright <brennandboatwright@gmail.com>
2024-11-21 11:22:18 -0600

    Sprint 3: Added AJAX calls to itemlist webpage

--- Sprint3Todo.md
+++ Sprint3Todo.md
@@ -1,13 +1,14 @@
 # Sprint 3 To-do
 
-- [ ] Account Creation (create.html, create.js)
+- [X] Account Creation (create.html, create.js)
 - [ ] Logging In (login.js)
 - [ ] Logging Out (logout.js)
 
-- [ ] Buyer Item List (itemlist.html, itemlist.js)
-- [ ] Seller Item List (item.html, item.js)
-- [ ] Admin Item List (adminitemlist.html, adminitemlist.js)
-
+- [X] Buyer Item List (itemlist.html, itemlist.js)
 - [ ] User Profile (profile.html, profile.js)
 
+- [ ] Debug the First 5 To-do Items
 - [ ] Turn in Video on Friday
+
+- [ ] Seller Item List (item.html, item.js)
+- [ ] Admin Item List (adminitemlist.html, adminitemlist.js)
--- frontend/itemlist.js
+++ frontend/itemlist.js
@@ -1,88 +1,67 @@
-const absoluteUrl = "http://dankpadserver.jordynsblog.org:5000/";
-//const absoluteUrl = "./";
-
 var orderId = -1;
 var orderStatus = -1;
 
-function CreateOrder() {
-    fetch(absoluteUrl + "order/create")
-        .then(
-            response => {
-                orderId = response.id;
-                orderStatus = response.orderstatus;
-                document.getElementById("order-message").innerText = `Order ${orderId} created with status ${orderStatus}.`;
-            }
-        )
-        .catch(
-            (e) => {
-                console.error(e.message);
-            }
-        );
-}
+$(document).ready(function () {
+    $.ajax({
+        type: "GET",
+        url: "http://dankpadserver.jordynsblog.org:5000/item/list",
+        success: onGetItemList()
+    });
+    $("#delete-order-button").click(deleteOrder());
+});
 
-function DeleteOrder() {
-    fetch(absoluteUrl + `order/${orderId}/delete`)
-        .then(
-            response => {
-                CreateOrder();
-                GenerateItemList();
-            }
-        )
-        .catch(
-            (e) => {
-                console.error(e.message);
-            }
-        );
+function deleteOrder() {
+    $.ajax({
+        type: "DELETE",
+        url: `http://dankpadserver.jordynsblog.org:5000/order/${orderId}/delete`,
+        success: onGetItemList()
+    });
 }
 
 function addItem(itemId) {
-    fetch(absoluteUrl + `order/${orderId}/add/${itemId}/1`)
-        .catch(
-            (e) => {
-                console.error(e.message);
-            }
-        );
+    $.ajax({
+        type: "POST",
+        url: `http://dankpadserver.jordynsblog.org:5000/order/${orderId}/add/${itemId}/1`
+    });
 }
 
 function deleteItem(itemId) {
-    fetch(absoluteUrl + `order/${orderId}/delete/${itemId}/1`)
-        .catch(
-            (e) => {
-                console.error(e.message);
-            }
-        );
+    $.ajax({
+        type: "DELETE",
+        url: `http://dankpadserver.jordynsblog.org:5000/order/${orderId}/delete/${itemId}/1`
+    });
 }
 
-function GenerateItemList() {
-    const itemListElement = document.getElementById("item-list");
-    if (!itemListElement)
-        return;
-    fetch(absoluteUrl + "item/list")
-        .then(
-            response => {
-                var itemListText = "";
-                let data = response.json();
-                for (let item of data.items) {
-                    itemListText += "<ul>\n";
-                    itemListText += `<li>Name: ${item.name}</li>\n`;
-                    itemListText += `<li>Quantity: ${item.quantity}</li>\n`;
-                    itemListText += `<li>Description: ${item.description}</li>\n`;
-                    itemListText += `<li>ID: ${item.id}</li>\n`;
-                    itemListText += `<li>Approved: ${item.approval}</li>\n`;
-                    itemListText += "</ul>\n<br/>\n";
-                    itemListText += `<button type="button" id="button1-${item.id}" onclick="addItem(${item.id})">Add?</button>\n`;
-                    itemListText += `<button type="button" id="button2-${item.id}" onclick="deleteItem(${item.id})>Delete?</button>\n`;
-                    itemListText += "<br/>\n";
-                }
-                itemListElement.innerHTML = itemListText;
+function onGetItemList() {
+    $.ajax({
+        type: "POST",
+        url: "http://dankpadserver.jordynsblog.org:5000/order/create",
+        success: function (response) {
+            let data = response.json();
+            orderId = data.id;
+            orderStatus = data.orderstatus; // Is this correct?
+            $("#order-messsage").innerText = `Order ${orderId} created with status ${orderStatus}.`;
+        }
+    });
+    $.ajax({
+        type: "GET",
+        url: "http://dankpadserver.jordynsblog.org:5000/item/list",
+        success: function (response) {
+            let data = response.json();
+            var itemListText = "";
+            for (let item of data.items) {
+                itemListText += "<ul>\n";
+                itemListText += `<li>Name: ${item.name}</li>\n`;
+                itemListText += `<li>Quantity: ${item.quantity}</li>\n`;
+                itemListText += `<li>Description: ${item.description}</li>\n`;
+                itemListText += `<li>ID: ${item.id}</li>\n`;
+                itemListText += `<li>Approved: ${item.approval}</li>\n`;
+                itemListText += "</ul>\n<br/>\n";
+                itemListText += `<button type="button" id="button1-${item.id}" onclick="addItem(${item.id})">Add?</button>\n`;
+                itemListText += `<button type="button" id="button2-${item.id}" onclick="deleteItem(${item.id})>Delete?</button>\n`;
+                itemListText += "<br/>\n";
             }
-        )
-        .catch(
-            (e) => {
-                console.error(e.message);
-            }
-        );
+            $("#item-list").html(itemListText);
+        }
+    });
 }
-
-CreateOrder();
-GenerateItemList();
