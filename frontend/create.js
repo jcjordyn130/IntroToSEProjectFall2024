cc0ecab94a53d36a0d2fb310611297a85cc9f148
Brennan D Boatwright <brennandboatwright@gmail.com>
2024-11-20 22:24:52 -0600

    Sprint 3: Implementing ajax post request in create.js

--- frontend/create.js
+++ frontend/create.js
@@ -1,6 +1,27 @@
-const absoluteUrl = "http://dankpadserver.jordynsblog.org:5000/";
-//const absoluteUrl = "./";
+$(document).ready(function () {
+    $("#create-acc-submit").click(function () {
+        const fd = new FormData(document.getElementById("create-form"));
+        const fdUsername = fd.get("username");
+        const fdPassword = fd.get("password");
+        const fdEmailAddress = fd.get("email-address");
+        const fdUserLevel = fd.get("user-level");
+        $.ajax({
+            type: "POST",
+            url: "http://dankpadserver.jordynsblog.org:5000/user/" + fdUsername + "/create",
+            contentType: "application/json",
+            data: {
+                email: fdEmailAddress,
+                password: fdPassword,
+                userlevel: fdUserLevel
+            },
+            success: function () {
+                document.getElementById("create-form-div").style.display = "none";
+            }
+        });
+    });
+});
 
+/*
 function FormCreate() {
     const fd = new FormData(document.getElementById("create-form"));
     const fdUsername = fd.get("username");
@@ -8,16 +29,18 @@ function FormCreate() {
     const fdEmailAddress = fd.get("email-address");
     const fdUserLevel = fd.get("user-level");
     const options = {
+        headers: { "Content-Type": "application/json; charset=utf-8" },
+        mode: "no-cors",
         method: "POST",
-        headers: { "content-type": "application/json" },
-        mode: "no-cors"
+        body: JSON.stringify({
+            email: fdEmailAddress,
+            password: fdPassword,
+            userlevel: fdUserLevel
+        })
     };
-    options.body = JSON.stringify({
-        email: fdEmailAddress,
-        password: fdPassword,
-        userlevel: fdUserLevel
-    });
 
+    // Put jquery post function here.
+    $.post(absoluteUrl + "user/" + fdUsername + "/create", FormCreateSuccess())
     fetch(absoluteUrl + "user/" + fdUsername + "/create", options)
         .then(
             response => {
@@ -32,3 +55,4 @@ function FormCreate() {
             }
         );
 }
+*/
