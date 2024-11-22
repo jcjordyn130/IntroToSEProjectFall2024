f1e762fb2ece7ea812e1382ab86da4b9201195cb
Brennan D Boatwright <brennandboatwright@gmail.com>
2024-11-22 16:42:13 -0600

    Sprint 3: modified user(list).js to use Ajax

--- frontend/user.js
+++ frontend/user.js
@@ -1,71 +1,4 @@
-const absoluteUrl = "http://dankpadserver.jordynsblog.org:5000";
-//const absoluteUrl = ".";
-
-function userAlreadyApproved() {
-    console.log("User Approval Already Set");
-}
-
-function approveUser() {
-    if (!username || !userInfoElement)
-        return;
-    fetch(absoluteUrl + "/user/" + username + "/approve")
-        .then(
-            response => {
-                buttonElement1.onclick = "userAlreadyApproved()";
-                buttonElement1.innerText = "User Approved";
-                buttonElement2.style.display = "none";
-            }
-        )
-        .catch(
-            (e) => {
-                console.error(e.message);
-            }
-        );
-}
-
-function unapproveuser() {
-    if (!username || !userInfoElement)
-        return;
-    fetch(absoluteUrl + "/user/" + username + "/unapprove")
-        .then(
-            response => {
-                buttonElement1.style.display = "none";
-                buttonElement2.onclick = "userAlreadyApproved()";
-                buttonElement2.innerText = "User Unapproved";
-            }
-        )
-        .catch(
-            (e) => {
-                console.error(e.message);
-            }
-        );
-}
-
-function UserApproveForm() {
-    if (!username || !userInfoElement)
-        return;
-
-    if (usernameElement)
-        usernameElement.innerText = username;
-    fetch(absoluteUrl + "/user/info/" + username)
-        .then(
-            response => {
-                let data = response.json();
-                var userInfoText = `<li>ID: ${data.id}</li>\n`;
-                userInfoText += `<li>Email: ${data.email}</li>\n`;
-                userInfoText += `<li>Level: ${data.userlevel}</li>\n`;
-                userInfoText = "<ul>\n" + userInfoText + "</ul>\n";
-                userInfoText += `<button id="button1" onclick="approveUser()">Approved?</button>\n`;
-                userInfoText += `<button id="button2" onclick="unapproveUser()">Unapproved?</button>\n`;
-                userInfoElement.innerHTML = userInfoText;
-            }
-        )
-        .catch(
-            (e) => {
-                console.error(e.message);
-            }
-        );
-}
+import "https://code.jquery.com/jquery-3.7.1.min.js";
 
 var username = null;
 for (const [k, v] of URLSearchParams()) {
@@ -78,4 +11,63 @@ const userInfoElement = document.getElementById("userinfo");
 const buttonElement1 = document.getElementById("button1"); // approve
 const buttonElement2 = document.getElementById("button2"); // unapprove
 
-UserApproveForm();
+function approveUser() {
+    if (!username || !userInfoElement)
+        return;
+    $.ajax({
+        type: "GET",
+        url: `http://dankpadserver.jordynsblog.org:5000/user/${username}/approve`,
+        data: JSON.stringify({
+            key: localStorage.getItem("apikey") // Is this correct?
+        }),
+        dataType: "json",
+        success: function () {
+            buttonElement1.onclick = `console.log("User Approval Already Set")`;
+            buttonElement1.innerText = "User Approved";
+            buttonElement2.style.display = "none";
+        }
+    });
+}
+
+function unapproveuser() {
+    if (!username || !userInfoElement)
+        return;
+    $.ajax({
+        type: "GET",
+        url: `http://dankpadserver.jordynsblog.org:5000/user/${username}/unapprove`,
+        data: JSON.stringify({
+            key: localStorage.getItem("apikey") // Is this correct?
+        }),
+        dataType: "json",
+        success: function () {
+            buttonElement1.style.display = "none";
+            buttonElement2.onclick = `console.log("User Approval Already Set")`;
+            buttonElement2.innerText = "User Unapproved"
+        }
+    });
+}
+
+$(document).ready(function () {
+    if (!username || !userInfoElement)
+        return;
+    if (usernameElement)
+        usernameElement.innerText = username;
+    $.ajax({
+        type: "GET",
+        url: "http://dankpadserver.jordynsblog.org:5000/user/info/" + username,
+        data: JSON.stringify({
+            key: localStorage.getItem("apikey") // Is this correct?
+        }),
+        dataType: "json",
+        success: function (response) {
+            let data = response.json().users[0]; // one item in list
+            var userInfoText = `<li>ID: ${data.id}</li>\n`;
+            userInfoText += `<li>Email: ${data.email}</li>\n`;
+            userInfoText += `<li>Level: ${data.userlevel}</li>\n`;
+            userInfoText = "<ul>\n" + userInfoText + "</ul>\n";
+            userInfoText += `<button id="button1" onclick="approveUser()">Approved?</button>\n`;
+            userInfoText += `<button id="button2" onclick="unapproveUser()">Unapproved?</button>\n`;
+            userInfoElement.innerHTML = userInfoText;
+        }
+    });
+});
--- frontend/userlist.js
+++ frontend/userlist.js
@@ -1,3 +1,5 @@
+import "https://code.jquery.com/jquery-3.7.1.min.js";
+
 $(document).ready(function () {
     const userListElement = document.getElementById("user-list");
     if (userListElement) {
@@ -5,7 +7,7 @@ $(document).ready(function () {
             type: "GET",
             url: "http://dankpadserver.jordynsblog.org:5000/user/list",
             data: JSON.stringify({
-                apikey: localStorage.getItem("apikey") // Is this correct?
+                key: localStorage.getItem("apikey") // Is this correct?
             }),
             dataType: "json",
             success: function (response) {
@@ -27,35 +29,3 @@ $(document).ready(function () {
         });
     }
 });
-
-/*
-const absoluteUrl = "http://dankpadserver.jordynsblog.org:5000";
-//const absoluteUrl = ".";
-var userListElement = document.getElementById("user-list");
-if (userListElement) {
-    fetch(absoluteUrl + "/user/list")
-        .then(
-            response => {
-                var userListText = "";
-                let data = response.json().users;
-                for (let user of data) {
-                    userListText = "<ul>\n";
-                    userListText += `<li>ID: ${user.id}</li>\n`;
-                    userListText += `<li>Email: ${user.email}</li>\n`;
-                    userListText += `<li>Level: ${user.userlevel}</li>\n`;
-                    userListText += "</ul>\n\n";
-                    // =====
-                    const userParams = new URLSearchParams();
-                    userParams.append("username", user.username);
-                    userListText += `<a href="user.html?${userParams.toString()}"></a>`
-                }
-                userListElement.innerHTML = userListText;
-            }
-        )
-        .catch(
-            (e) => {
-                console.error(e.message);
-            }
-        );
-}
-*/
