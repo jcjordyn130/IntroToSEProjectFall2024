cd5f033c932eab5d6a0c3d135d7cd73051df2241
MadduxLofton <125148911+MadduxLofton@users.noreply.github.com>
2024-11-21 18:40:17 -0600

    Sprint 3: Fixed create.html submit printing create.html
    the script needs to be called before a function can be used for it.

--- frontend/create.js
+++ frontend/create.js
@@ -1,20 +1,25 @@
-$(document).ready(function () {
-    $("#create-acc-submit").click(function () {
+$(document).ready(function()
+{
+    $("#create-acc-submit").click(function ()
+    {
         const fd = new FormData(document.getElementById("create-form"));
         const fdUsername = fd.get("username");
         const fdPassword = fd.get("password");
         const fdEmailAddress = fd.get("email-address");
         const fdUserLevel = fd.get("user-level");
-        $.ajax({
+        $.ajax(
+        {
             type: "POST",
             url: "http://dankpadserver.jordynsblog.org:5000/user/" + fdUsername + "/create",
             contentType: "application/json",
-            data: {
+            data:
+            {
                 email: fdEmailAddress,
                 password: fdPassword,
                 userlevel: fdUserLevel
             },
-            success: function () {
+            success: function ()
+            {
                 document.getElementById("create-form-div").style.display = "none";
             }
         });
@@ -30,6 +35,19 @@ function validateForm() // adding the "required" tag to the elements made this f
         return false;
     }
 }
+
+// This function needs to call the api to make a new account with the information from the form
+function submit_form()
+{
+    //let username = document.forms["create-form"]["username"].value;
+    //let password = document.forms["create-form"]["password"].value;
+    //let email = document.forms["create-form"]["email"].value;
+    //let user_type = document.forms["create-form"]["user-type"].value;
+    alert("Hello World");
+    return;
+}
+
+//submit_form();
 
 /*
 function FormCreate() {
